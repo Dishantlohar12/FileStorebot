1d5641d0772645c55c0a5dfd75963095bcdf492b
Dishantlohar12 <malviyadishant729@gmail.com>
2026-02-16 19:35:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,39 +4,39 @@ from logging.handlers import RotatingFileHandler
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
 PORT = '5010'
-OWNER_ID = 6497757690
+OWNER_ID = 5904478052
 
 MSG_EFFECT = 5046509860389126442
 
-SHORT_URL = "linkshortify.com" # shortner url 
+SHORT_URL = "" # shortner url 
 SHORT_API = "" 
-SHORT_TUT = "https://t.me/How_to_Download_7x/26"
+SHORT_TUT = ""
 
 # Bot Configuration
-SESSION = "yato"
+SESSION = "dishant"
 TOKEN = "642712"
-API_ID = ""
-API_HASH = ""
-WORKERS = 5
+API_ID = "26590590"
+API_HASH = "4805ee1d57b1be7f5135e736c816a2d1"
+WORKERS = 2
 
 DB_URI = "mongodb"
-DB_NAME = "yato"
+DB_NAME = "dishantdb"
 
 FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL = -1003778789033   # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
 #     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
 # }
 # Auto Delete Timer (seconds)
-AUTO_DEL = 300
+AUTO_DEL = 
 # Admin IDs
 ADMINS = [6497757690, 6103092779]
 # Bot Settings
 DISABLE_BTN = True
-PROTECT = True
+PROTECT = False
 
 # Messages Configuration
 MESSAGES = {
